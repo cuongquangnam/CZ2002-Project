12c565c4312c36bf57f36c9af7662f674a320df2
cuongquangnam <42933311+cuongquangnam@users.noreply.github.com>
2019-11-10 03:46:50 +0800

    Update Cinema.java

--- Cinema.java
+++ Cinema.java
@@ -1,17 +1,19 @@
 package com.models;
 
 public class Cinema {
-	
+	private static final int row = 9;
+	private static final int col = 16;
 	private Cineplex cineplex;
 	private boolean is3D;
 	private boolean isPlatinum;
+	private Seat[][] seats;
 	
 	public Cinema(Cineplex cineplex, boolean is3D, boolean isPlatinum) {
 		this.cineplex = cineplex;
 		this.is3D = is3D;
 		this.isPlatinum = isPlatinum;
+		this.seats = new Seat[row][col];
 	}
-	
 	public Cineplex getCineplex() {
 		return cineplex;
 	}
