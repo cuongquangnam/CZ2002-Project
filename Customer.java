f10905127c067406e51eb112396eccb5213d8b79
cuongquangnam <42933311+cuongquangnam@users.noreply.github.com>
2019-11-09 17:21:24 +0800

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -2,13 +2,23 @@ package com.models;
 
 public class Customer {
 	
+	private int customer_id;
 	private int age;
 	private String name;
 	private String mobile;
 	private String email;
 	private boolean isStudent;
+	private BookingHistory bookingHistory;
 	
-	
+	public Customer (int customer_id, int age, String name, String mobile, String email, boolean isStudent) {
+		this.customer_id = customer_id;
+		this.age = age;
+		this.name = name;
+		this.mobile = mobile;
+		this.email = email;
+		this.isStudent = isStudent;
+		this.bookingHistory = new BookingHistory(this);
+	}
 	public int getAge() {
 		return age;
 	}
@@ -39,9 +49,17 @@ public class Customer {
 	public void setStudent(boolean isStudent) {
 		this.isStudent = isStudent;
 	}
-
-	
-	
-	
-
+	public int getCustomer_id() {
+		return customer_id;
+	}
+	public void setCustomer_id(int customer_id) {
+		this.customer_id = customer_id;
+	}
+	public BookingHistory getBookingHistory() {
+		return bookingHistory;
+	}
 }
+	
+	
+	
+
