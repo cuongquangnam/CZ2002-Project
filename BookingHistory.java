a3743267fcd74f340c2585b7428f4d48cfcc7bba
cuongquangnam <42933311+cuongquangnam@users.noreply.github.com>
2019-11-09 17:22:11 +0800

    Update BookingHistory.java

--- BookingHistory.java
+++ BookingHistory.java
@@ -1,5 +1,20 @@
 package com.models;
 
-public class BookingHistory {
+import java.util.ArrayList;
 
+public class BookingHistory {
+	private Customer customer;
+	private ArrayList<BookingandPayment> bookingHistory;
+	
+	public BookingHistory(Customer customer) {
+		this.customer = customer;
+		bookingHistory = new ArrayList<BookingandPayment>();
+	}
+	public void addBookingHistory(BookingandPayment bp) {
+		if (bp.getCustomer().getCustomer_id()== customer.getCustomer_id())
+			bookingHistory.add(bp);
+	}
+	public String toString() {
+		return this.customer.getName();
+	}
 }
