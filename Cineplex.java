d23d4a9d32394afcaec2b2c8953eb1f5638a2d26
cuongquangnam <42933311+cuongquangnam@users.noreply.github.com>
2019-11-11 18:08:45 +0800

    Update Cineplex.java

--- Cineplex.java
+++ Cineplex.java
@@ -1,16 +1,24 @@
-package com.models;
+package models;
+
+import java.util.ArrayList;
+
 
 public class Cineplex {
 	
     private String staffusername;
 	private String staffpassword;
+	private ArrayList<Cinema> cinemas;
 	
-	
-	public Cineplex (String staffusername, String staffpassword)
+	public Cineplex (String staffusername, String staffpassword, int numCinema)
 	{
 	this.staffusername = staffusername;
     this.staffpassword = staffpassword;
+    this.cinemas = new ArrayList<Cinema>();
    }
+	
+	public void addCinema(boolean is3D, boolean isPlatinum) {
+		cinemas.add(new Cinema(this,is3D,isPlatinum));
+	}
 
 	public String getStaffusername() {
 		return staffusername;
