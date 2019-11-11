68f02db66a629abfa70bd16914889b16fa2c4e46
ritwikkanodia <44768122+ritwikkanodia@users.noreply.github.com>
2019-11-11 19:00:13 +0800

    Add files via upload

--- LoginController.java
+++ LoginController.java
@@ -1,28 +1,67 @@
 package controller;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 import model.*;
 import view.*;
 public class LoginController {
 	
-	public static void  getUsername() 
+	static Scanner sc = new Scanner(System.in);
+	static String username;
+	static String password;
+	static int cineplex;
+	
+	
+
+	public static void  inputUsername() 
 	{
-		Scanner sc = new Scanner(System.in);
-		String username = sc.next();
+		
+	username = sc.next();
 	}
 	
-	public static void getPassword() 
+	public static void inputPassword() 
 	{
-		Scanner sc = new Scanner(System.in);
-		String password = sc.next();
+	
+	password = sc.next();
 	}
 	
-	LoginService service = new LoginService();
+	public static void inputCineplex()
+	{
+	cineplex = sc.nextInt();
+	}
 	
-	public boolean isUserValid(Cineplex c, String username, String password)
+	static LoginService service = new LoginService();
+	
+	public static boolean isUserValid(Cineplex c, String username, String password)
 	{
 		boolean validate = service.validateUser(c, username, password);
 		return validate;
 	}
 	
+	public static void main(String args[]) throws IOException
+	{
+		LoginView loginview = new LoginView();
+		loginview.initialize();
+		CineplexController.writeToFile() ;
 
-}
+		     
+		String FILENAME_CINEPLEX = "src/DataFolder/cineplexdata.dat";
+			   try {
+				@SuppressWarnings("unchecked")
+				ArrayList <Cineplex> cineplexList= (ArrayList<Cineplex>) DataController.readSerializedObject(FILENAME_CINEPLEX);
+				System.out.println(cineplexList);
+				boolean b = isUserValid(cineplexList.get(0), username, password);
+				System.out.println(b);
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				
+			}
+			   
+			 
+	}
+		
+	}
+	
+
+
