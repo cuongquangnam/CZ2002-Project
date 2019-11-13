438b04ca5a7044179ea4e12de6ac3000956ba58b
Ta Anh Duc <43318919+anhduc001@users.noreply.github.com>
2019-11-13 18:36:51 +0800

    Add files via upload

---
+++ MOBLIMA.java
@@ -0,0 +1,10 @@
+import view.*;
+
+import java.io.IOException;
+
+public class MOBLIMA {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+	// write your code here
+        new MainView().initialize();
+    }
+}
