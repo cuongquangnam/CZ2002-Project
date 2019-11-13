095c02f3c6c22ae7349be1e8362069c98fe71ee2
ritwikkanodia <44768122+ritwikkanodia@users.noreply.github.com>
2019-11-14 07:15:13 +0800

    Add files via upload

--- Model 14/Seat.java
+++ Model 14/Seat.java
@@ -1,73 +1,29 @@
-package model;
+package com.models;
 
-import java.io.Serializable;
+public class Seat {
+	
+	int seatRow;
+	int seatColumn;
+	public Seat(int seatRow, int seatColumn) {
+		super();
+		this.seatRow = seatRow;
+		this.seatColumn = seatColumn;
+	}
+	@Override
+	public String toString() {
+		return "Seat [seatRow=" + seatRow + ", seatColumn=" + seatColumn + "]";
+	}
+	public int getSeatRow() {
+		return seatRow;
+	}
+	public void setSeatRow(int seatRow) {
+		this.seatRow = seatRow;
+	}
+	public int getSeatColumn() {
+		return seatColumn;
+	}
+	public void setSeatColumn(int seatColumn) {
+		this.seatColumn = seatColumn;
+	}
 
-/**
- * This class contains all information of a seat - including its row number, column number,
- * {@code Show} and whether it is booked
- */
-public class Seat implements Serializable{
-    private final int row;
-    private final int col;
-    private final Show showtime;
-    private boolean booked;
-
-    /**
-     * Allocates a {@code Seat} object and initializes it specified by its row number, column
-     * number and showtime.
-     * @param row This it the row number of the seat
-     * @param col This is the column number of the seat
-     * @param showtime This is the showtime of the seat
-     */
-    public Seat(int row, int col, Show showtime) {
-        this.row = row;
-        this.col = col;
-        this.showtime = showtime;
-        booked = false;
-    }
-
-    /**
-     * This method is to get the showtime}.
-     * @return showtime of the seat
-     */
-    public Show getShowtime() {
-        return showtime;
-    }
-
-    /**
-     * This method is to get column number.
-     * @return column number
-     */
-    public int getCol() {
-        return col;
-    }
-
-    /**
-     * This method is to get row number.
-     * @return row number
-     */
-    public int getRow() {
-        return row;
-    }
-
-    /**
-     * This method is to get a boolean value which indicates whether the seat is booked.
-     * @return true if the seat is booked, false if the seat is not booked
-     */
-    public boolean isBooked() {
-        return booked;
-    }
-
-    /**
-     * This method is to book the seat by setting {@code booked} as true
-     */
-    public void bookSeat() {
-        booked = true;
-    }
-
-    @Override
-    public String toString() {
-        if (!booked) return "[ ]";
-        else return "[*]";
-    }
 }
