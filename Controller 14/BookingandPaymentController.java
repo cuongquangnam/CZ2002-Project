da74cf605fb4e11c1a894a77d9158e4e96d62793
ritwikkanodia <44768122+ritwikkanodia@users.noreply.github.com>
2019-11-13 21:48:29 +0800

    Add files via upload

---
+++ Controller 14/BookingandPaymentController.java
@@ -0,0 +1,28 @@
+package com.controllers;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+
+import com.models.Booking;
+import com.models.Movie;
+import com.models.Seat;
+import com.models.Show;
+
+public class BookingandPaymentController {
+	
+	public static void createNewBooking(Date booking_dt, Movie movie, Show show, ArrayList <Seat> seat, String TID,  String name,  String mobileNumber, String emailID)
+	{
+		Booking booking  = new Booking( booking_dt,  movie,  show,  seat,  TID,   name,   mobileNumber,  emailID);
+		System.out.println(booking);
+		
+	}
+	
+		
+			
+		
+		
+
+		
+}
