07ebb651a918b08e9e75929e50e23fa79b0f37c3
Ta Anh Duc <43318919+anhduc001@users.noreply.github.com>
2019-11-14 01:23:04 +0800

    Add files via upload

--- Model 14/Seat.java
+++ Model 14/Seat.java
@@ -2,13 +2,23 @@ package model;
 
 import java.io.Serializable;
 
+/**
+ * This class contains all information of a seat - including its row number, column number,
+ * {@code Show} and whether it is booked
+ */
 public class Seat implements Serializable{
     private final int row;
     private final int col;
     private final Show showtime;
     private boolean booked;
 
-
+    /**
+     * Allocates a {@code Seat} object and initializes it specified by its row number, column
+     * number and showtime.
+     * @param row This it the row number of the seat
+     * @param col This is the column number of the seat
+     * @param showtime This is the showtime of the seat
+     */
     public Seat(int row, int col, Show showtime) {
         this.row = row;
         this.col = col;
@@ -16,27 +26,41 @@ public class Seat implements Serializable{
         booked = false;
     }
 
-
+    /**
+     * This method is to get the showtime}.
+     * @return showtime of the seat
+     */
     public Show getShowtime() {
         return showtime;
     }
 
-
+    /**
+     * This method is to get column number.
+     * @return column number
+     */
     public int getCol() {
         return col;
     }
 
-
+    /**
+     * This method is to get row number.
+     * @return row number
+     */
     public int getRow() {
         return row;
     }
 
-
+    /**
+     * This method is to get a boolean value which indicates whether the seat is booked.
+     * @return true if the seat is booked, false if the seat is not booked
+     */
     public boolean isBooked() {
         return booked;
     }
 
-
+    /**
+     * This method is to book the seat by setting {@code booked} as true
+     */
     public void bookSeat() {
         booked = true;
     }
