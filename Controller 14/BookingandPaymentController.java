505461f4952e094aa47f08bdea604375cd8f4001
ritwikkanodia <44768122+ritwikkanodia@users.noreply.github.com>
2019-11-14 07:16:16 +0800

    Update BookingandPaymentController.java

--- Controller 14/BookingandPaymentController.java
+++ Controller 14/BookingandPaymentController.java
@@ -14,6 +14,9 @@ public class BookingandPaymentController {
 	
 	public static void createNewBooking(Date booking_dt, Movie movie, Show show, ArrayList <Seat> seat, String TID,  String name,  String mobileNumber, String emailID)
 	{
+		for(Seat s : seat) {
+			show.getSeatmap().setSeatArr(s.getSeatRow(), s.getSeatColumn());
+		}
 		Booking booking  = new Booking( booking_dt,  movie,  show,  seat,  TID,   name,   mobileNumber,  emailID);
 		System.out.println(booking);
 		
