56f1a9bfb67875199dafc44f13ef5c7bf2e2af5a
ritwikkanodia <44768122+ritwikkanodia@users.noreply.github.com>
2019-11-13 21:49:12 +0800

    Add files via upload

--- Model 14/Booking.java
+++ Model 14/Booking.java
@@ -1,23 +1,67 @@
-package model;
+package com.models;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Booking {
-
-	private Customer customer;
+	
+	private Date booking_dt;
+	private Movie movie;
+	private Show show;
+	private ArrayList <Seat> seat;
 	private String TID;
-	private Seat seat;
-	private String code;
-
-	public String getCode() {
-		return code;
+    private String name;
+	private String mobileNumber;
+	private String emailID;
+	
+	
+	public Booking()
+	{
+	
 	}
-	public void setCode(String code) {
-		this.code = code;
+	
+	public Booking(Date booking_dt, Movie movie, Show show, ArrayList <Seat> seat, String tID, String name,
+			String mobileNumber, String emailID) {
+		super();
+		this.booking_dt = booking_dt;
+		this.movie = movie;
+		this.show = show;
+		this.seat = seat;
+		this.TID = tID;
+		this.name = name;
+		this.mobileNumber = mobileNumber;
+		this.emailID = emailID;
 	}
-	public Customer getCustomer() {
-		return customer;
+	
+	
+	@Override
+	public String toString() {
+		return "BookingAndPayment [booking_dt=" + booking_dt + ", movie=" + movie + ", show=" + show + ", seat=" + seat
+				+ ", TID=" + TID + ", name=" + name + ", mobileNumber=" + mobileNumber + ", emailID=" + emailID + "]";
 	}
-	public void setCustomer(Customer customer) {
-		this.customer = customer;
+	public Date getBooking_dt() {
+		return booking_dt;
+	}
+	public void setBooking_dt(Date booking_dt) {
+		this.booking_dt = booking_dt;
+	}
+	public Movie getMovie() {
+		return movie;
+	}
+	public void setMovie(Movie movie) {
+		this.movie = movie;
+	}
+	public Show getShow() {
+		return show;
+	}
+	public void setShow(Show show) {
+		this.show = show;
+	}
+	public ArrayList <Seat> getSeat() {
+		return seat;
+	}
+	public void setSeat(ArrayList <Seat> seat) {
+		this.seat = seat;
 	}
 	public String getTID() {
 		return TID;
@@ -25,11 +69,26 @@ public class Booking {
 	public void setTID(String tID) {
 		TID = tID;
 	}
-	public Seat getSeat() {
-		return seat;
+	public String getName() {
+		return name;
 	}
-	public void setSeat(Seat seat) {
-		this.seat = seat;
+	public void setName(String name) {
+		this.name = name;
 	}
-
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+	public String getEmailID() {
+		return emailID;
+	}
+	public void setEmailID(String emailID) {
+		this.emailID = emailID;
+	}
 }
+	
+	
+	
+	
