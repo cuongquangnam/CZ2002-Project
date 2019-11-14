ca84298560fa14f10e415e446cc0e933fd3a0f7f
ritwikkanodia <44768122+ritwikkanodia@users.noreply.github.com>
2019-11-14 20:17:14 +0800

    Add files via upload

--- MOBLIMA/src/model/TicketCostService.java
+++ MOBLIMA/src/model/TicketCostService.java
@@ -1,26 +1,107 @@
-////Maybe implement change in price for seat section
-//
-//package model;
-//
-//public class TicketCostService {
-//	private double totalCost;
-//
-//	//function to calculate total cost of 1 movie ticket
-//	public double totalCostTicket(Customer customer, Cinema cinema, Movie movie)
-//	{
-//		totalCost = movie.getBasePrice();
-//		if(customer.isStudent() == true)
-//			totalCost += -10;
-//
-//		if(cinema.is3D() == true)
-//			totalCost += 5;
-//
-//		if(cinema.isPlatinum() == true)
-//			totalCost += 5;
-//
-//		return totalCost;
-//
-//	}
-//
-//
-//}
+package model;
+
+import java.util.ArrayList;
+
+public class TicketCostService
+{
+	
+	public Movie getMovie() {
+		return movie;
+	}
+
+
+
+
+	public void setMovie(Movie movie) {
+		this.movie = movie;
+	}
+
+
+
+
+	public Show getShow() {
+		return show;
+	}
+
+
+
+
+	public void setShow(Show show) {
+		this.show = show;
+	}
+
+
+
+
+	public Seat getSeat() {
+		return seat;
+	}
+
+
+
+
+	public void setSeat(Seat seat) {
+		this.seat = seat;
+	}
+
+
+
+
+	public double getPrice() {
+		return price;
+	}
+
+
+
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+
+
+
+	public TicketCostService(Movie movie, Show show, Seat seat, double price) {
+		super();
+		this.movie = movie;
+		this.show = show;
+		this.seat = seat;
+		this.price = price;
+	}
+
+
+
+
+	Movie movie;
+	Show show;
+	Seat seat; //Put an age attribute in seat
+	double price;
+	
+	
+	
+	
+	public double  calculateTicketPriceSeat(Movie movie, ArrayList<Seat> seats, Show show, Cinema cinema)
+	{
+		
+		price =movie.getBasePrice();
+		
+		if(movie.isBlockBuster() == true)
+			price += 5;
+		for(Seat s : seats)
+		{
+			if(s.isSenior() == true)
+				price -=5;
+		}
+		if(show.getTime() == //enum class of holidays)
+			price += 5;
+		
+		if(cinema.is3D() == true)
+			price += 5;
+		
+		if(cinema.isPlatinum() == true)
+			price += 5;
+		
+		return price;
+		
+	}
+}
