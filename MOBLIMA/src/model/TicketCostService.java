ea10d6c1cab5cb6c24db3223a8ea54d1dbb92424
cuongquangnam <42933311+cuongquangnam@users.noreply.github.com>
2019-11-15 20:48:45 +0800

    Update TicketCostService.java

--- MOBLIMA/src/model/TicketCostService.java
+++ MOBLIMA/src/model/TicketCostService.java
@@ -1,15 +1,11 @@
 package model;
 
-import controller.FileReadWriteController;
 import controller.TicketPriceController;
 
 import java.util.ArrayList;
 
 public class TicketCostService
 {
-    Movie movie;
-    Show show;
-    Seat seat;
     static double price;
     static double priceBlockBuster = TicketPriceController.getBlockBusterPrice();
     static double price3D = TicketPriceController.get3DPrice();
@@ -17,37 +13,6 @@ public class TicketCostService
     static double pricePlatinum = TicketPriceController.getPlatinumPrice();
 //    static double priceHoliday = TicketPriceController.getHolidayPrice();
 
-    public TicketCostService(Movie movie, Show show, Seat seat, double price) {
-        super();
-        this.movie = movie;
-        this.show = show;
-        this.seat = seat;
-        this.price = price;
-    }
-    public Movie getMovie() {
-        return movie;
-    }
-    public void setMovie(Movie movie) {
-        this.movie = movie;
-    }
-    public Show getShow() {
-        return show;
-    }
-    public void setShow(Show show) {
-        this.show = show;
-    }
-    public Seat getSeat() {
-        return seat;
-    }
-    public void setSeat(Seat seat) {
-        this.seat = seat;
-    }
-    public double getPrice() {
-        return price;
-    }
-    public void setPrice(double price) {
-        this.price = price;
-    }
     public static double  calculateTicketPriceSeat(Movie movie, ArrayList<Seat> seats, Show show, Cinema cinema)
     {
 
