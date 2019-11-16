233d1bfc8c021e58356d4a990e8ab35390eb60f1
ritwikkanodia <44768122+ritwikkanodia@users.noreply.github.com>
2019-11-17 02:51:17 +0800

    Update SystemSettingView.java

--- MOBLIMA/src/view/admin/SystemSettingView.java
+++ MOBLIMA/src/view/admin/SystemSettingView.java
@@ -6,11 +6,20 @@ import controller.ViewController;
 import java.io.IOException;
 
 public class SystemSettingView extends ViewController {
+    //View to guide the user through changing the system configuration process, provide correct inputs and provide feedback.
+    /**
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
     @Override
     public void initialize() throws IOException, ClassNotFoundException {
         displayMenu();
     }
-
+/**
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+    //Transfer the control to the correct view which is designed to guide the user based on user choice
     private void displayMenu() throws IOException, ClassNotFoundException {
         while(true) {
             IOController.printTitle("System setting");
