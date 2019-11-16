f53b07fa9162b12fb7690fa8e53827cfc71300ad
ritwikkanodia <44768122+ritwikkanodia@users.noreply.github.com>
2019-11-17 02:36:33 +0800

    Update DeleteMovieView.java

--- MOBLIMA/src/view/admin/DeleteMovieView.java
+++ MOBLIMA/src/view/admin/DeleteMovieView.java
@@ -14,16 +14,25 @@ import java.util.HashMap;
 import static controller.IOController.*;
 
 public class DeleteMovieView extends ViewController {
+   //View to guide the user through the Movie deletion process, provide correct inputs and provide feedback.
+     /**
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
     @Override
     public void initialize() throws IOException, ClassNotFoundException {
         displayMenu();
     }
 
+     /**
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
     private void displayMenu(){
         printTitle("Delete movie listing");
 
-        Movie movie = new Movie();
-        ArrayList<Movie> movieList = MovieListingController.getAllMovie();
+        Movie movie = new Movie(); //To store the movie to be deleted 
+        ArrayList<Movie> movieList = MovieListingController.getAllMovie(); //ArrayList of all the movies
         if(movieList.size() == 0)
             System.out.println("There is no movie yet!");
         else {
