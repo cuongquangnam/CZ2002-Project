4d28e2a94172b540af218a7c56a544e6b59711e7
ritwikkanodia <44768122+ritwikkanodia@users.noreply.github.com>
2019-11-17 02:11:44 +0800

    Update Customer.java

--- MOBLIMA/src/model/Customer.java
+++ MOBLIMA/src/model/Customer.java
@@ -4,24 +4,23 @@ package model;
  * This class contains information on the customer
  */
 public class Customer {
-	/**
-	 * Name of customer
-	 */
+	
 	private String name;
-	/**
-	 * Mobile phone of customer
-	 */
 	private String mobile;
-	/**
-	 * Email of
-	 */
 	private String email;
 	
+	/**
+	 * @param name		Name of the Customer.
+	 * @param mobile	Mobile number of the customer.
+	 * @param email		Email ID of the customer.
+	 */
 	public Customer (String name, String mobile, String email){
 		this.name = name;
 		this.mobile = mobile;
 		this.email = email;
 	}
+	
+	//Getter and Setter methods for the attributes of the Customer class.
 	public String getName() {
 		return name;
 	}
@@ -43,5 +42,3 @@ public class Customer {
 }
 	
 	
-	
-
